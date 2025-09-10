e6a309f9c56252ab9d33da241ff2ca4344716671
AYOMIDE FAMOYEGUN <ayomidefamoyegun1@gmail.com>
2025-09-10 22:41:51 +0000

    okay

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -8,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDWVrgv7maLXPrkrGg_wVFG7PtiEpUREFo",
   authDomain: "dev-habit-quest.firebaseapp.com",
-  projectId: "dev-habit-quest",
+  projectId: "habit-quest-148",
   storageBucket: "dev-habit-quest.appspot.com",
   messagingSenderId: "105559642375",
   appId: "1:105559642375:web:8e3d3e6d8c634e72355e2e"
